b9033bf20d3f68fc6a39368ac8315dbd1b0c0a3d
mauriziogiuffredi91 <giuffro91@gmail.com>
2021-06-08 22:54:08 +0200

    comics page

--- resources/views/comics.blade.php
+++ resources/views/comics.blade.php
@@ -7,12 +7,27 @@
     <div class="boxes">
         @foreach ($cards as $card )
             <div class="card">
-                <img src="{{$card['image']}}" alt="">
-                <h4 class="title-comics">{{$card['title']}}</h4>
+
+                <div class="box-img">
+
+                    <img src="{{$card['image']}}" alt="comics">
+
+                </div>
+                <h5 class="title-comics">{{$card['title']}}</h5>
+                <h4 class="price" >{{$card['price']}}€</h4>
             </div>
         @endforeach
 
     </div>
+
+    <div class="bottom-section">
+
+        <button class="load"> <a href="/">Load More</a> </button>
+
+    </div>
+
+       
+   
 
 </section>
 @endsection
