69084a8fbd3ca55d2c1068be7f7506b19aa23751
mauriziogiuffredi91 <giuffro91@gmail.com>
2021-06-08 16:35:07 +0200

    footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,3 +1,29 @@
 <footer>
+    <div class="container">
+        <div class="button-gray">
+            <a  href="/">Sign up now!</a>
 
+        </div>
+
+        <div class="footer-right">
+            <a class="special-a" href="/">Follow Us</a>
+
+            <a class="special-a" href="/">
+                <img src="{{asset('img/footer-facebook.png')}}" alt="facebook">
+            </a>
+            <a class="special-a" href="/">
+                <img src="{{asset('img/footer-twitter.png')}}" alt="twitter">
+            </a>
+            <a class="special-a" href="/">
+                <img src="{{asset('img/footer-youtube.png')}}" alt="youtube">
+            </a>
+            <a class="special-a" href="/">
+                <img src="{{asset('img/footer-pinterest.png')}}" alt="pinterest">
+            </a>
+            <a class="special-a" href="/">
+                <img src="{{asset('img/footer-periscope.png')}}" alt="periscope">
+            </a>
+
+        </div>
+    </div>
 </footer>
